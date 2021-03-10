eca16cd63f5f2c656f760b8533de332f39c5bada
kyjin8 <kyjin9705@gmail.com>
2021-03-10 17:41:45 +0900

    app 수정
    수정

--- test/myapp/app.js
+++ test/myapp/app.js
@@ -28,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
+app.use('/logout', logoutRouter);
 app.use('/signup', signupRouter);
 app.use('/users', usersRouter);
 
