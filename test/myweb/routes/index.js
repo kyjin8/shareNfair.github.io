b2555d934e3a0290e0e8210960da5194c05c1d87
KangYeJin <kyjin8@naver.com>
2021-03-13 02:05:35 +0900

    대공사
    모듈화
    시간 라이브러리
    가격콤마 함수
    게시글 눌렀을 때 페이지

--- test/myweb/routes/index.js
+++ test/myweb/routes/index.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 const session = require('./session');
 const client = require('./mysql');
+const moment = require('moment');
 
 router.use(session)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   client.query('select * from posts ORDER BY id', (err, results) => {
-    res.render('index', {login: req.session.userid, posts: results})
+    res.render('index', {login: req.session.userid, posts: results, moment: moment})
   })
 });
 
